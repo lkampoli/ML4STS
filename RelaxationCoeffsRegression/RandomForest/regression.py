1f5d66d648f1f33d534d92a4644ad2de05d827b2
lcampoli <l.kampoli@spbu.ru>
2020-08-24 20:57:25 +0300

    added D, VT, VV dataset in RF

--- RelaxationCoeffsRegression/RandomForest/regression.py
+++ RelaxationCoeffsRegression/RandomForest/regression.py
@@ -26,35 +26,33 @@ from sklearn.pipeline import Pipeline
 n_jobs = -1
 trial  = 1
 
-dataset=np.loadtxt("../data/datarelax.txt")
+#dataset=np.loadtxt("../data/datarelax.txt")
+#dataset=np.loadtxt("../data/datasetDR.txt")
+#dataset=np.loadtxt("../data/datasetVT.txt")
+dataset=np.loadtxt("../data/datasetVV.txt")
 
 # ... only for plotting
 #dataset=np.loadtxt("../data/datarelax.txt")
-#x=dataset[:,0:1]   # Temperatures
-#y=dataset[:,1:50]  # Rci (relaxation source terms)
-
-#for i in range (2,48):
-#    plt.scatter(dataset[:,0:1], dataset[:,i], s=0.5, label=i)
-
+#dataset=np.loadtxt("../data/datasetDR.txt")
+#dataset=np.loadtxt("../data/datasetVT.txt")
+#dataset=np.loadtxt("../data/datasetVV.txt")
+#x=dataset[:,2:3]   # Temperatures
+#y=dataset[:,3:50]  # Rci (relaxation source terms)
+#
+#for i in range (3,48):
+#    plt.scatter(dataset[:,2:3], dataset[:,i], s=0.5, label=i)
+#
 #plt.title('$R_{ci}$ for $N_2/N$')
 #plt.xlabel('T [K]')
 #plt.ylabel('$R_{ci}$ $[J/m^3/s]$')
-##plt.legend()
+#plt.legend()
 #plt.tight_layout()
 #plt.savefig("relaxation_source_terms.pdf")
 #plt.show()
 
 # Here, I learn one specific level of R_ci spanning all temperatures
-x=dataset[:,0:1]   # Temperatures
-y=dataset[:,9:10]  # Rci (relaxation source terms)
-
-# Here, I fix the temperature and learn all levels of R_ci
-#x=dataset[150,0:1]   # Temperatures
-#y=dataset[150,1:50]  # Rci (relaxation source terms)
-
-# TODO: Here, I want to learn all T and all Rci alltogether
-#x=dataset[:,0:1]   # Temperatures
-#y=dataset[:,1:50]  # Rci (relaxation source terms)
+x=dataset[:,2:3]   # Temperatures
+y=dataset[:,30:31]  # Rci (relaxation source terms)
 
 # 2D Plot
 #plt.scatter(x, y, s=0.5)
@@ -72,7 +70,7 @@ y=dataset[:,9:10]  # Rci (relaxation source terms)
 #Y = sc_y.fit_transform(y)
 
 #x_train, x_test, y_train, y_test = train_test_split(X, Y, train_size=0.75, test_size=0.25, random_state=42)
-x_train_sc, x_test_sc, y_train_sc, y_test_sc = train_test_split(x, y, train_size=0.80, test_size=0.20, random_state=42)
+x_train_sc, x_test_sc, y_train_sc, y_test_sc = train_test_split(x, y, train_size=0.70, test_size=0.30, random_state=0)
 
 sc_x = StandardScaler()
 sc_y = StandardScaler()
@@ -89,12 +87,15 @@ print('Testing Labels Shape:', y_test.shape)
 
 # Random Forest
 hyper_params = [{
+                 #'n_estimators': (1, 10, 100,),
                  'n_estimators': (1, 10, 100, 1000,),
                  #'n_estimators': (1, 10, 100, 1000, 10000),
                  'min_weight_fraction_leaf': (0.0, 0.01, 0.02,),
                  #'min_weight_fraction_leaf': (0.0, 0.25, 0.5, 0.75, 0.9),
+                 #'max_features': ('auto',),
                  'max_features': ('auto','sqrt','log2'),
-                 'criterion': ('mse', 'mae'),
+                 #'criterion': ('mse', 'mae'),
+                 'criterion': ('mse',),
                   #'max_samples': (0.1, 0.25, 0.5, 0.75, 0.9,),
                   #'bootstrap': (True, False),
                   #'warm_start': (True, False),
@@ -104,7 +105,7 @@ hyper_params = [{
 
 est=ensemble.RandomForestRegressor()
 
-gs = GridSearchCV(est, cv=10, param_grid=hyper_params, verbose=2, n_jobs=n_jobs, scoring='r2')
+gs = GridSearchCV(est, cv=5, param_grid=hyper_params, verbose=2, n_jobs=n_jobs, scoring='r2')
 
 t0 = time.time()
 gs.fit(x_train, y_train.ravel())
@@ -123,14 +124,12 @@ test_score_mse  = mean_squared_error(      sc_y.inverse_transform(y_test),  sc_y
 test_score_mae  = mean_absolute_error(     sc_y.inverse_transform(y_test),  sc_y.inverse_transform(gs.predict(x_test)))
 test_score_evs  = explained_variance_score(sc_y.inverse_transform(y_test),  sc_y.inverse_transform(gs.predict(x_test)))
 test_score_me   = max_error(               sc_y.inverse_transform(y_test),  sc_y.inverse_transform(gs.predict(x_test)))
-test_score_msle = mean_squared_log_error(  sc_y.inverse_transform(y_test),  sc_y.inverse_transform(gs.predict(x_test)))
 test_score_r2   = r2_score(                sc_y.inverse_transform(y_test),  sc_y.inverse_transform(gs.predict(x_test)))
 
 print("The model performance for testing set")
 print("--------------------------------------")
 print('MAE is {}'.format(test_score_mae))
 print('MSE is {}'.format(test_score_mse))
-print('MSLE is {}'.format(test_score_msle))
 print('EVS is {}'.format(test_score_evs))
 print('ME is {}'.format(test_score_me))
 print('R2 score is {}'.format(test_score_r2))
@@ -152,18 +151,6 @@ out_text = '\t'.join(['regression',
 print(out_text)
 sys.stdout.flush()
 
-# KernelRidge
-#best_algorithm   = gs.best_params_['algorithm']
-#best_n_neighbors = gs.best_params_['n_neighbors']
-#best_leaf_size   = gs.best_params_['leaf_size']
-#best_weights     = gs.best_params_['weights']
-#best_p           = gs.best_params_['p']
-
-# kNearestNeighbour
-#best_kernel       = gs.best_params_['kernel']
-#best_alpha        = gs.best_params_['alpha']
-#best_gamma        = gs.best_params_['gamma']
-
 # RandomForest
 best_n_estimators = gs.best_params_['n_estimators']
 best_min_weight_fraction_leaf = gs.best_params_['min_weight_fraction_leaf']
@@ -171,25 +158,12 @@ best_max_features = gs.best_params_['max_features']
 best_criterion = gs.best_params_['criterion']
 
 outF = open("output.txt", "w")
-#print('best_algorithm = ', best_algorithm, file=outF)
-#print('best_n_neighbors = ', best_n_neighbors, file=outF)
-#print('best_leaf_size = ', best_leaf_size, file=outF)
-#print('best_weights = ', best_weights, file=outF)
-#print('best_p = ', best_p, file=outF)
-#
-#print('best_kernel = ', best_kernel, file=outF)
-#print('best_alpha = ', best_alpha, file=outF)
-#print('best_gamma = ', best_gamma, file=outF)
-#
 print('best_n_estimators = ', best_n_estimators, file=outF)
 print('best_min_weight_fraction_leaf = ', best_min_weight_fraction_leaf, file=outF)
 print('best_max_features = ', best_max_features, file=outF)
 print('best_criterion = ', best_criterion, file=outF)
 outF.close()
 
-#regr = KNeighborsRegressor(n_neighbors=best_n_neighbors, algorithm=best_algorithm,
-#                         leaf_size=best_leaf_size, weights=best_weights, p=best_p)
-#regr = KernelRidge(kernel=best_kernel, gamma=best_gamma, alpha=best_alpha)
 regr = RandomForestRegressor(n_estimators=best_n_estimators,
                              min_weight_fraction_leaf=best_min_weight_fraction_leaf,
                              max_features=best_max_features,
