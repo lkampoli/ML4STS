detected bug 'data leaking' due to normalization of all data

RelaxationCoeffsRegression/KernelRidge/regression.py
@@ -18,12 +18,12 @@ from sklearn.preprocessing import MinMaxScaler, StandardScaler
 from sklearn.model_selection import train_test_split, GridSearchCV, learning_curve, cross_val_score
 from sklearn import kernel_ridge
 from sklearn.kernel_ridge import KernelRidge
-from sklearn.neighbors import KNeighborsRegressor
-from sklearn.neighbors import RadiusNeighborsRegressor
-from sklearn import neighbors
-from sklearn.neighbors import NearestNeighbors
+from joblib import dump, load
+import pickle
+from sklearn.multioutput import MultiOutputRegressor
+from sklearn.pipeline import Pipeline
 
-n_jobs = 1
+n_jobs = -1
 trial  = 1
 
 dataset=np.loadtxt("../data/datarelax.txt")
@@ -65,33 +65,39 @@ y=dataset[:,9:10]  # Rci (relaxation source terms)
 #plt.savefig("relaxation_source_terms.pdf")
 #plt.show()
 
-y=np.reshape(y, (-1,1))
+#y=np.reshape(y, (-1,1))
+#sc_x = StandardScaler()
+#sc_y = StandardScaler()
+#X = sc_x.fit_transform(x)
+#Y = sc_y.fit_transform(y)
+
+#x_train, x_test, y_train, y_test = train_test_split(X, Y, train_size=0.75, test_size=0.25, random_state=42)
+x_train_sc, x_test_sc, y_train_sc, y_test_sc = train_test_split(x, y, train_size=0.75, test_size=0.25, random_state=42)
+
 sc_x = StandardScaler()
 sc_y = StandardScaler()
-X = sc_x.fit_transform(x)
-Y = sc_y.fit_transform(y)
 
-x_train, x_test, y_train, y_test = train_test_split(X, Y, train_size=0.75, test_size=0.25, random_state=42)
+x_train = sc_x.fit_transform(x_train_sc)
+y_train = sc_y.fit_transform(y_train_sc)
+x_test  = sc_x.fit_transform(x_test_sc)
+y_test  = sc_y.fit_transform(y_test_sc)
 
 print('Training Features Shape:', x_train.shape)
 print('Training Labels Shape:', y_train.shape)
 print('Testing Features Shape:', x_test.shape)
 print('Testing Labels Shape:', y_test.shape)
 
-hyper_params = [{'kernel': ('poly','rbf',), 'alpha': (1e-4,1e-2,0.1,1,10,), 'gamma': (0.01,0.1,1,10,100,),}]
-#hyper_params = [{'algorithm': ('ball_tree', 'kd_tree', 'brute',), 'n_neighbors': (1,2,3,4,5,6,7,8,9,10,),
-#                 'leaf_size': (1, 10, 20, 30, 100,), 'weights': ('uniform', 'distance',), 'p': (1,2,),}]
+#hyper_params = [{'kernel': ('poly','rbf',), 'alpha': (1e-4,1e-3,1e-2,0.1,1,10,100,1000,), 'gamma': (0.001,0.01,0.1,1,10,100,1000,),}]
+hyper_params = [{'kernel': ('poly','rbf',), 'alpha': (1e-6,1e-5,1e-4,1e-3,1e-2,0.1,1,), 'gamma': (0.001,0.01,0.1,1,10,100,1000,),}]
 
 est=kernel_ridge.KernelRidge()
-#est=neighbors.NearestNeighbors()
-#est=neighbors.KNeighborsRegressor()
 
-gs = GridSearchCV(est, cv=5, param_grid=hyper_params, verbose=2, n_jobs=n_jobs, scoring='r2')
+gs = GridSearchCV(est, cv=10, param_grid=hyper_params, verbose=2, n_jobs=n_jobs, scoring='r2')
 
 t0 = time.time()
 gs.fit(x_train, y_train.ravel())
 runtime = time.time() - t0
-print("kNN complexity and bandwidth selected and model fitted in %.3f s" % runtime)
+print("kNN complexity and bandwidth selected and model fitted in %.6f s" % runtime)
 
 train_score_mse = mean_squared_error(      sc_y.inverse_transform(y_train), sc_y.inverse_transform(gs.predict(x_train)))
 train_score_mae = mean_absolute_error(     sc_y.inverse_transform(y_train), sc_y.inverse_transform(gs.predict(x_train)))
@@ -120,28 +126,16 @@ out_text = '\t'.join(['regression',
 print(out_text)
 sys.stdout.flush()
 
-#best_algorithm   = gs.best_params_['algorithm']
-#best_n_neighbors = gs.best_params_['n_neighbors']
-#best_leaf_size   = gs.best_params_['leaf_size']
-#best_weights     = gs.best_params_['weights']
-#best_p           = gs.best_params_['p']
 best_kernel       = gs.best_params_['kernel']
 best_alpha        = gs.best_params_['alpha']
 best_gamma        = gs.best_params_['gamma']
 
 outF = open("output.txt", "w")
-#print('best_algorithm = ', best_algorithm, file=outF)
-#print('best_n_neighbors = ', best_n_neighbors, file=outF)
-#print('best_leaf_size = ', best_leaf_size, file=outF)
-#print('best_weights = ', best_weights, file=outF)
-#print('best_p = ', best_p, file=outF)
 print('best_kernel = ', best_kernel, file=outF)
 print('best_alpha = ', best_alpha, file=outF)
 print('best_gamma = ', best_gamma, file=outF)
 outF.close()
 
-#kn = KNeighborsRegressor(n_neighbors=best_n_neighbors, algorithm=best_algorithm,
-#                         leaf_size=best_leaf_size, weights=best_weights, p=best_p)
 regr = KernelRidge(kernel=best_kernel, gamma=best_gamma, alpha=best_alpha)
 
 t0 = time.time()

RelaxationCoeffsRegression/MultiLayerPerceptron/regression.py
@@ -17,8 +17,12 @@ from sklearn import preprocessing
 from sklearn.preprocessing import MinMaxScaler, StandardScaler
 from sklearn.model_selection import train_test_split, GridSearchCV, learning_curve, cross_val_score
 from sklearn.neural_network import MLPRegressor
+from sklearn.multioutput import MultiOutputRegressor
+from sklearn.pipeline import Pipeline
+from joblib import dump, load
+import pickle
 
-n_jobs = 1
+n_jobs = -1
 trial  = 1
 
 dataset=np.loadtxt("../data/datarelax.txt")
@@ -60,50 +64,27 @@ y=dataset[:,9:10]  # Rci (relaxation source terms)
 #plt.savefig("relaxation_source_terms.pdf")
 #plt.show()
 
-y=np.reshape(y, (-1,1))
+#y=np.reshape(y, (-1,1))
+#sc_x = StandardScaler()
+#sc_y = StandardScaler()
+#X = sc_x.fit_transform(x)
+#Y = sc_y.fit_transform(y)
+
+#x_train, x_test, y_train, y_test = train_test_split(X, Y, train_size=0.75, test_size=0.25, random_state=42)
+x_train_sc, x_test_sc, y_train_sc, y_test_sc = train_test_split(x, y, train_size=0.80, test_size=0.20, random_state=42)
+
 sc_x = StandardScaler()
 sc_y = StandardScaler()
-X = sc_x.fit_transform(x)
-Y = sc_y.fit_transform(y)
 
-x_train, x_test, y_train, y_test = train_test_split(X, Y, train_size=0.75, test_size=0.25, random_state=42)
+x_train = sc_x.fit_transform(x_train_sc)
+y_train = sc_y.fit_transform(y_train_sc)
+x_test  = sc_x.fit_transform(x_test_sc)
+y_test  = sc_y.fit_transform(y_test_sc)
 
 print('Training Features Shape:', x_train.shape)
 print('Training Labels Shape:', y_train.shape)
 print('Testing Features Shape:', x_test.shape)
 print('Testing Labels Shape:', y_test.shape)
-
-# KernelRidge
-#hyper_params = [{'kernel': ('poly','rbf',), 'alpha': (1e-4,1e-2,0.1,1,10,), 'gamma': (0.01,0.1,1,10,100,),}]
-
-# k-kearest neighbor
-#hyper_params = [{'algorithm': ('ball_tree', 'kd_tree', 'brute',), 'n_neighbors': (1,2,3,4,5,6,7,8,9,10,),
-#                 'leaf_size': (1, 10, 20, 30, 100,), 'weights': ('uniform', 'distance',), 'p': (1,2,),}]
-
-# Random Forest
-#hyper_params = [{'n_estimators': (10, 100, 1000),
-#                 'min_weight_fraction_leaf': (0.0, 0.25, 0.5),
-#                 'max_features': ('sqrt','log2',None),
-#}]
-
-# Extra Trees
-#hyper_params = [{'n_estimators': (10, 100, 1000,),
-#                 'min_weight_fraction_leaf': (0.0, 0.25, 0.5,),
-#                 'max_features': ('sqrt','log2','auto', None,),
-#                 'max_samples': (1,10,100,1000,),
-#                 'bootstrap': (True, False,),
-#                 'oob_score': (True, False,),
-#                 'warm_start': (True, False,),
-#                 'criterion': ('mse', 'mae',),
-#                 'max_depth': (1,10,100,None,),
-#                 'max_leaf_nodes': (1,10,100,),
-#                 'min_samples_split': (0.1,0.25,0.5,0.75,1.0,),
-#                 'min_samples_leaf': (1,10,100,),
-#}]
-
-# Support Vector Machines
-#hyper_params = [{'kernel': ('poly', 'rbf',), 'gamma': ('scale', 'auto',),
-#                 'C': (1e-2, 1e-1, 1e0, 1e1, 1e2,), 'epsilon': (1e-2, 1e-1, 1e0, 1e1, 1e2,), }]
 
 # MultiLayerPerceptron
 hyper_params = [{
@@ -114,20 +95,17 @@ hyper_params = [{
         'nesterovs_momentum': (True, False,),
 },]
 
-#est=ensemble.RandomForestRegressor()
-#est=kernel_ridge.KernelRidge()
-#est=neighbors.NearestNeighbors()
-#est=neighbors.KNeighborsRegressor()
-#est=ensemble.ExtraTreesRegressor()
-#est=svm.SVR()
 est=MLPRegressor()
 
-gs = GridSearchCV(est, cv=5, param_grid=hyper_params, verbose=2, n_jobs=n_jobs, scoring='r2')
+gs = GridSearchCV(est, cv=10, param_grid=hyper_params, verbose=2, n_jobs=n_jobs, scoring='r2')
 
 t0 = time.time()
 gs.fit(x_train, y_train.ravel())
 runtime = time.time() - t0
 print("Complexity and bandwidth selected and model fitted in %.6f s" % runtime)
+
+# save the model to disk
+dump(gs, 'model_MLP.sav')
 
 train_score_mse = mean_squared_error(      sc_y.inverse_transform(y_train), sc_y.inverse_transform(gs.predict(x_train)))
 train_score_mae = mean_absolute_error(     sc_y.inverse_transform(y_train), sc_y.inverse_transform(gs.predict(x_train)))
@@ -140,6 +118,9 @@ test_score_evs  = explained_variance_score(sc_y.inverse_transform(y_test),  sc_y
 test_score_me   = max_error(               sc_y.inverse_transform(y_test),  sc_y.inverse_transform(gs.predict(x_test)))
 
 sorted_grid_params = sorted(gs.best_params_.items(), key=operator.itemgetter(0))
+
+#feature_importances = gs.best_estimator_.feature_importances_
+#print(feature_importances)
 
 out_text = '\t'.join(['regression',
                       str(trial),
@@ -156,44 +137,6 @@ out_text = '\t'.join(['regression',
 print(out_text)
 sys.stdout.flush()
 
-# KernelRidge
-#best_algorithm   = gs.best_params_['algorithm']
-#best_n_neighbors = gs.best_params_['n_neighbors']
-#best_leaf_size   = gs.best_params_['leaf_size']
-#best_weights     = gs.best_params_['weights']
-#best_p           = gs.best_params_['p']
-
-# kNearestNeighbour
-#best_kernel       = gs.best_params_['kernel']
-#best_alpha        = gs.best_params_['alpha']
-#best_gamma        = gs.best_params_['gamma']
-
-# RandomForest
-#best_n_estimators = gs.best_params_['n_estimators']
-#best_min_weight_fraction_leaf = gs.best_params_['min_weight_fraction_leaf']
-#best_max_features = gs.best_params_['max_features']
-
-# ExtraTrees
-#best_n_estimators = gs.best_params_['n_estimators']
-#best_min_weight_fraction_leaf = gs.best_params_['min_weight_fraction_leaf']
-#best_max_features = gs.best_params_['max_features']
-#best_max_samples = gs.best_params_['max_samples']
-#best_bootstrap = gs.best_params_['bootstrap']
-#best_oob_score = gs.best_params_['oob_score']
-#best_warm_start = gs.best_params_['warm_start']
-#best_criterion = gs.best_params_['criterion']
-#best_max_depth = gs.best_params_['max_depth']
-#best_min_samples_split = gs.best_params_['min_samples_split']
-#best_min_samples_leaf = gs.best_params_['min_samples_leaf']
-#best_max_leaf_nodes = gs.best_params_['max_leaf_nodes']
-
-# SVR
-#best_kernel = gs.best_params_['kernel']
-#best_gamma = gs.best_params_['gamma']
-#best_C = gs.best_params_['C']
-#best_epsilon = gs.best_params_['epsilon']
-
-# MLP
 best_hidden_layer_sizes = gs.best_params_['hidden_layer_sizes']
 best_activation = gs.best_params_['activation']
 best_solver = gs.best_params_['solver']
@@ -201,38 +144,6 @@ best_learning_rate = gs.best_params_['learning_rate']
 best_nesterovs_momentum = gs.best_params_['nesterovs_momentum']
 
 outF = open("output.txt", "w")
-#print('best_algorithm = ', best_algorithm, file=outF)
-#print('best_n_neighbors = ', best_n_neighbors, file=outF)
-#print('best_leaf_size = ', best_leaf_size, file=outF)
-#print('best_weights = ', best_weights, file=outF)
-#print('best_p = ', best_p, file=outF)
-#
-#print('best_kernel = ', best_kernel, file=outF)
-#print('best_alpha = ', best_alpha, file=outF)
-#print('best_gamma = ', best_gamma, file=outF)
-#
-#print('best_n_estimators = ', best_n_estimators, file=outF)
-#print('best_min_weight_fraction_leaf = ', best_min_weight_fraction_leaf, file=outF)
-#print('best_max_features = ', best_max_features, file=outF)
-#
-#print('best_n_estimators = ', best_n_estimators, file=outF)
-#print('best_min_weight_fraction_leaf = ', best_min_weight_fraction_leaf, file=outF)
-#print('best_max_features = ', best_max_features, file=outF)
-#print('best_bootstrap = ', best_bootstrap, file=outF)
-#print('best_oob_score = ', best_oob_score, file=outF)
-#print('best_warm_start = ', best_warm_start, file=outF)
-#print('best_criterion = ', best_criterion, file=outF)
-#print('best_max_depth = ', best_max_depth, file=outF)
-#print('best_min_samples_split = ', best_min_samples_split, file=outF)
-#print('best_min_samples_leaf = ', best_min_samples_leaf, file=outF)
-#print('best_min_samples_leaf = ', best_min_samples_leaf, file=outF)
-#print('best_max_leaf_nodes = ', best_max_leaf_nodes, file=outF)
-#
-#print('best_kernel = ', best_kernel, file=outF)
-#print('best_gamma = ', best_gamma, file=outF)
-#print('best_C = ', best_C, file=outF)
-#print('best_epsilon = ', best_epsilon, file=outF)
-#
 print('best_hidden_layer_sizes = ', best_hidden_layer_sizes, file=outF)
 print('best_activation = ', best_activation, file=outF)
 print('best_solver = ', best_solver, file=outF)
@@ -240,31 +151,12 @@ print('best_learning_rate = ', best_learning_rate, file=outF)
 print('best_nesterovs_momentum = ', best_nesterovs_momentum, file=outF)
 outF.close()
 
-#regr = KNeighborsRegressor(n_neighbors=best_n_neighbors, algorithm=best_algorithm,
-#                         leaf_size=best_leaf_size, weights=best_weights, p=best_p)
-#
-#regr = KernelRidge(kernel=best_kernel, gamma=best_gamma, alpha=best_alpha)
-#
-#regr = RandomForestRegressor(n_estimators=best_n_estimators,
-#                             min_weight_fraction_leaf=best_min_weight_fraction_leaf,
-#                             max_features=best_max_features)
-#
-#regr = ExtraTreesRegressor(n_estimators=best_n_estimators,
-#                           min_weight_fraction_leaf=best_min_weight_fraction_leaf,
-#                           max_features=best_max_features,
-#                           bootstrap=best_bootstrap,
-#                           oob_score=best_oob_score,
-#                           warm_start=best_warm_start,
-#                           criterion=best_criterion,
-#                           max_depth=best_max_depth,
-#                           max_leaf_nodes=best_max_leaf_nodes,
-#                           min_samples_split=best_min_samples_split,
-#                           min_samples_leaf=best_min_samples_leaf)
-#
-#regr = SVR(kernel=best_kernel, epsilon=best_epsilon, C=best_C, gamma=best_gamma)
-#
-regr = MLPRegressor(hidden_layer_sizes=best_hidden_layer_sizes, activation=best_activation, solver=best_solver,
-                    learning_rate=best_learning_rate, nesterovs_momentum=best_nesterovs_momentum, max_iter=1000)
+regr = MLPRegressor(hidden_layer_sizes=best_hidden_layer_sizes,
+                    activation=best_activation,
+                    solver=best_solver,
+                    learning_rate=best_learning_rate,
+                    nesterovs_momentum=best_nesterovs_momentum,
+                    max_iter=1000)
 
 t0 = time.time()
 regr.fit(x_train, y_train.ravel())

RelaxationCoeffsRegression/RandomForest/regression.py
@@ -20,6 +20,8 @@ from sklearn import ensemble
 from sklearn.ensemble import RandomForestRegressor
 from joblib import dump, load
 import pickle
+from sklearn.multioutput import MultiOutputRegressor
+from sklearn.pipeline import Pipeline
 
 n_jobs = -1
 trial  = 1
@@ -63,29 +65,32 @@ y=dataset[:,9:10]  # Rci (relaxation source terms)
 #plt.savefig("relaxation_source_terms.pdf")
 #plt.show()
 
-y=np.reshape(y, (-1,1))
+#y=np.reshape(y, (-1,1))
+#sc_x = StandardScaler()
+#sc_y = StandardScaler()
+#X = sc_x.fit_transform(x)
+#Y = sc_y.fit_transform(y)
+
+#x_train, x_test, y_train, y_test = train_test_split(X, Y, train_size=0.75, test_size=0.25, random_state=42)
+x_train_sc, x_test_sc, y_train_sc, y_test_sc = train_test_split(x, y, train_size=0.80, test_size=0.20, random_state=42)
+
 sc_x = StandardScaler()
 sc_y = StandardScaler()
-X = sc_x.fit_transform(x)
-Y = sc_y.fit_transform(y)
 
-x_train, x_test, y_train, y_test = train_test_split(X, Y, train_size=0.75, test_size=0.25, random_state=42)
+x_train = sc_x.fit_transform(x_train_sc)
+y_train = sc_y.fit_transform(y_train_sc)
+x_test  = sc_x.fit_transform(x_test_sc)
+y_test  = sc_y.fit_transform(y_test_sc)
 
 print('Training Features Shape:', x_train.shape)
 print('Training Labels Shape:', y_train.shape)
 print('Testing Features Shape:', x_test.shape)
 print('Testing Labels Shape:', y_test.shape)
 
-# KernelRidge
-#hyper_params = [{'kernel': ('poly','rbf',), 'alpha': (1e-4,1e-2,0.1,1,10,), 'gamma': (0.01,0.1,1,10,100,),}]
-
-# k-kearest neighbor
-#hyper_params = [{'algorithm': ('ball_tree', 'kd_tree', 'brute',), 'n_neighbors': (1,2,3,4,5,6,7,8,9,10,),
-#                 'leaf_size': (1, 10, 20, 30, 100,), 'weights': ('uniform', 'distance',), 'p': (1,2,),}]
-
 # Random Forest
 hyper_params = [{
-                 'n_estimators': (1, 10, 100, 1000, 10000),
+                 'n_estimators': (1, 10, 100, 1000,),
+                 #'n_estimators': (1, 10, 100, 1000, 10000),
                  'min_weight_fraction_leaf': (0.0, 0.01, 0.02,),
                  #'min_weight_fraction_leaf': (0.0, 0.25, 0.5, 0.75, 0.9),
                  'max_features': ('auto','sqrt','log2'),
@@ -95,13 +100,9 @@ hyper_params = [{
                   #'warm_start': (True, False),
                   #'min_impurity_decrease': (0.1, 0.25, 0.5. 0.75, 0.9,),
                   #'max_leaf_nodes': (1, 10, 100, 1000,),
-                  #'
 }]
 
 est=ensemble.RandomForestRegressor()
-#est=kernel_ridge.KernelRidge()
-#est=neighbors.NearestNeighbors()
-#est=neighbors.KNeighborsRegressor()
 
 gs = GridSearchCV(est, cv=10, param_grid=hyper_params, verbose=2, n_jobs=n_jobs, scoring='r2')
 

RelaxationCoeffsRegression/SupportVectorMachines/regression_MO.py
@@ -21,61 +21,25 @@ from sklearn.svm import SVR
 from joblib import dump, load
 import pickle
 from sklearn.multioutput import MultiOutputRegressor
+from sklearn.multioutput import RegressorChain
+from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import RepeatedKFold
 from sklearn.pipeline import Pipeline
+from numpy import mean
+from numpy import std
+from numpy import absolute
 
 n_jobs = 1
 trial  = 1
 
 dataset=np.loadtxt("../data/datarelax.txt")
 
-# ... only for plotting
-#dataset=np.loadtxt("../data/datarelax.txt")
-#x=dataset[:,0:1]   # Temperatures
-#y=dataset[:,1:50]  # Rci (relaxation source terms)
-
-#for i in range (2,48):
-#    plt.scatter(dataset[:,0:1], dataset[:,i], s=0.5, label=i)
-
-#plt.title('$R_{ci}$ for $N_2/N$')
-#plt.xlabel('T [K]')
-#plt.ylabel('$R_{ci}$ $[J/m^3/s]$')
-##plt.legend()
-#plt.tight_layout()
-#plt.savefig("relaxation_source_terms.pdf")
-#plt.show()
-
-# Here, I learn one specific level of R_ci spanning all temperatures
 x=dataset[:,0:1]  # Temperatures
-y=dataset[:,9:10] # Rci (relaxation source terms)
-
-# Here, I fix the temperature and learn all levels of R_ci
-#x=dataset[150,0:1]   # Temperatures
-#y=dataset[150,1:50]  # Rci (relaxation source terms)
-
-# TODO: Here, I want to learn all T and all Rci alltogether
-#x=dataset[:,0:1]   # Temperatures
-#y=dataset[:,1:50]  # Rci (relaxation source terms)
-
-# 2D Plot
-#plt.scatter(x, y, s=0.5)
-#plt.title('$R_{ci}$ for $N_2/N$ and i = 10')
-#plt.xlabel('T [K]')
-#plt.ylabel('$R_{ci}$ $[J/m^3/s]$')
-#plt.tight_layout()
-#plt.savefig("relaxation_source_terms.pdf")
-#plt.show()
-
-#y=np.reshape(y, (-1,1))
-#sc_x = StandardScaler()
-#sc_y = StandardScaler()
-#X = sc_x.fit_transform(x)
-#Y = sc_y.fit_transform(y)
+y=dataset[:,1:50] # Rci (relaxation source terms)
 
 #x_train, x_test, y_train, y_test = train_test_split(X, Y, train_size=0.75, test_size=0.25, random_state=42)
 #x_train_sc, x_test_sc, y_train_sc, y_test_sc = train_test_split(x, y, train_size=0.75, test_size=0.25, random_state=42)
 x_train_sc, x_test_sc, y_train_sc, y_test_sc = train_test_split(x, y, train_size=0.80, test_size=0.20, random_state=42)
-
-#y   = np.reshape(y, (-1,1))
 
 sc_x = StandardScaler()
 sc_y = StandardScaler()
@@ -109,38 +73,51 @@ hyper_params = [{
                  }]
 
 est = svm.SVR()
-print(est)
+regr = MultiOutputRegressor(estimator=est)
+#regr = RegressorChain(base_estimator=est)
 
-gs = GridSearchCV(est, cv=10, param_grid=hyper_params, verbose=2, n_jobs=n_jobs, scoring='r2')
+#gs = GridSearchCV(est, cv=10, param_grid=hyper_params, verbose=2, n_jobs=n_jobs, scoring='r2')
+
+# define the evaluation procedure
+cv = RepeatedKFold(n_splits=10, n_repeats=3, random_state=1)
+
+# evaluate the model and collect the scores
+n_scores = cross_val_score(regr, x_train, y_train, scoring='neg_mean_absolute_error', cv=cv, n_jobs=-1)
+#scores = cross_val_score(regr, x_train, y_train, scoring='accuracy', cv=cv, n_jobs=-1)
+
+# force the scores to be positive
+n_scores = absolute(n_scores)
+
+# summarize performance
+print('MAE: %.6f (%.6f)' % (mean(n_scores), std(n_scores)))
+#print('Accuracy: %.6f (%.6f)' % (mean(scores), std(scores)))
 
 t0 = time.time()
-gs.fit(x_train, y_train.ravel())
+gs.fit(x_train, y_train)
 runtime = time.time() - t0
 print("Complexity and bandwidth selected and model fitted in %.6f s" % runtime)
 
 # save the model to disk
-dump(gs, 'model_SVR.sav')
+dump(gs, 'model_MO_SVR.sav')
 
-train_score_mse = mean_squared_error(      sc_y.inverse_transform(y_train), sc_y.inverse_transform(gs.predict(x_train)))
-train_score_mae = mean_absolute_error(     sc_y.inverse_transform(y_train), sc_y.inverse_transform(gs.predict(x_train)))
-train_score_evs = explained_variance_score(sc_y.inverse_transform(y_train), sc_y.inverse_transform(gs.predict(x_train)))
-train_score_me  = max_error(               sc_y.inverse_transform(y_train), sc_y.inverse_transform(gs.predict(x_train)))
+#train_score_mse = mean_squared_error(      sc_y.inverse_transform(y_train), sc_y.inverse_transform(gs.predict(x_train)))
+#train_score_mae = mean_absolute_error(     sc_y.inverse_transform(y_train), sc_y.inverse_transform(gs.predict(x_train)))
+#train_score_evs = explained_variance_score(sc_y.inverse_transform(y_train), sc_y.inverse_transform(gs.predict(x_train)))
+#train_score_me  = max_error(               sc_y.inverse_transform(y_train), sc_y.inverse_transform(gs.predict(x_train)))
+#
+#test_score_mse  = mean_squared_error(      sc_y.inverse_transform(y_test),  sc_y.inverse_transform(gs.predict(x_test)))
+#test_score_mae  = mean_absolute_error(     sc_y.inverse_transform(y_test),  sc_y.inverse_transform(gs.predict(x_test)))
+#test_score_evs  = explained_variance_score(sc_y.inverse_transform(y_test),  sc_y.inverse_transform(gs.predict(x_test)))
+#test_score_me   = max_error(               sc_y.inverse_transform(y_test),  sc_y.inverse_transform(gs.predict(x_test)))
+#test_score_r2   = r2_score(                sc_y.inverse_transform(y_test),  sc_y.inverse_transform(gs.predict(x_test)))
 
-test_score_mse  = mean_squared_error(      sc_y.inverse_transform(y_test),  sc_y.inverse_transform(gs.predict(x_test)))
-test_score_mae  = mean_absolute_error(     sc_y.inverse_transform(y_test),  sc_y.inverse_transform(gs.predict(x_test)))
-#test_score_msle = mean_squared_log_error(  sc_y.inverse_transform(y_test),  sc_y.inverse_transform(gs.predict(x_test)))
-test_score_evs  = explained_variance_score(sc_y.inverse_transform(y_test),  sc_y.inverse_transform(gs.predict(x_test)))
-test_score_me   = max_error(               sc_y.inverse_transform(y_test),  sc_y.inverse_transform(gs.predict(x_test)))
-test_score_r2   = r2_score(                sc_y.inverse_transform(y_test),  sc_y.inverse_transform(gs.predict(x_test)))
-
-print("The model performance for testing set")
-print("--------------------------------------")
-print('MAE is {}'.format(test_score_mae))
-print('MSE is {}'.format(test_score_mse))
-#print('MSLE is {}'.format(test_score_msle))
-print('EVS is {}'.format(test_score_evs))
-print('ME is {}'.format(test_score_me))
-print('R2 score is {}'.format(test_score_r2))
+#print("The model performance for testing set")
+#print("--------------------------------------")
+#print('MAE is {}'.format(test_score_mae))
+#print('MSE is {}'.format(test_score_mse))
+#print('EVS is {}'.format(test_score_evs))
+#print('ME is {}'.format(test_score_me))
+#print('R2 score is {}'.format(test_score_r2))
 
 #feature_importances = gs.best_estimator_.feature_importances_
 #print(feature_importances)
@@ -302,3 +279,31 @@ plt.tight_layout()
 plt.savefig("regression_SVR.eps", dpi=150, crop='false')
 plt.savefig("regression_SVR.pdf", dpi=150, crop='false')
 plt.show()
+
+
+
+#from sklearn.multioutput import MultiOutputRegressor
+#from sklearn.svm import SVR
+#from sklearn.model_selection import GridSearchCV
+#from sklearn.pipeline import Pipeline
+#
+#pipe_svr = Pipeline([('scl', StandardScaler()),
+#        ('reg', MultiOutputRegressor(SVR()))])
+#
+#grid_param_svr = {
+#    'reg__estimator__C': [0.1,1,10]
+#}
+#
+#gs_svr = (GridSearchCV(estimator=pipe_svr,
+#                      param_grid=grid_param_svr,
+#                      cv=2,
+#                      scoring = 'neg_mean_squared_error',
+#                      n_jobs = -1))
+#
+#gs_svr = gs_svr.fit(X_train,y_train)
+#gs_svr.best_estimator_
+#
+#Pipeline(steps=[('scl', StandardScaler(copy=True, with_mean=True, with_std=True)),
+#('reg', MultiOutputRegressor(estimator=SVR(C=10, cache_size=200,
+# coef0=0.0, degree=3, epsilon=0.1, gamma='auto', kernel='rbf', max_iter=-1,
+# shrinking=True, tol=0.001, verbose=False), n_jobs=1))])

RelaxationCoeffsRegression/kNearestNeighbor/regression.py
@@ -16,14 +16,15 @@ from sklearn.metrics import *
 from sklearn import preprocessing
 from sklearn.preprocessing import MinMaxScaler, StandardScaler
 from sklearn.model_selection import train_test_split, GridSearchCV, learning_curve, cross_val_score
-from sklearn import kernel_ridge
-from sklearn.kernel_ridge import KernelRidge
+from sklearn import neighbors
 from sklearn.neighbors import KNeighborsRegressor
 from sklearn.neighbors import RadiusNeighborsRegressor
-from sklearn import neighbors
 from sklearn.neighbors import NearestNeighbors
+import pickle
+from sklearn.multioutput import MultiOutputRegressor
+from sklearn.pipeline import Pipeline
 
-n_jobs = 1
+n_jobs = -1
 trial  = 1
 
 dataset=np.loadtxt("../data/datarelax.txt")
@@ -65,13 +66,22 @@ y=dataset[:,9:10]  # Rci (relaxation source terms)
 #plt.savefig("relaxation_source_terms.pdf")
 #plt.show()
 
-y=np.reshape(y, (-1,1))
+#y=np.reshape(y, (-1,1))
+#sc_x = StandardScaler()
+#sc_y = StandardScaler()
+#X = sc_x.fit_transform(x)
+#Y = sc_y.fit_transform(y)
+
+#x_train, x_test, y_train, y_test = train_test_split(X, Y, train_size=0.75, test_size=0.25, random_state=42)
+x_train_sc, x_test_sc, y_train_sc, y_test_sc = train_test_split(x, y, train_size=0.80, test_size=0.20, random_state=42)
+
 sc_x = StandardScaler()
 sc_y = StandardScaler()
-X = sc_x.fit_transform(x)
-Y = sc_y.fit_transform(y)
 
-x_train, x_test, y_train, y_test = train_test_split(X, Y, train_size=0.75, test_size=0.25, random_state=42)
+x_train = sc_x.fit_transform(x_train_sc)
+y_train = sc_y.fit_transform(y_train_sc)
+x_test  = sc_x.fit_transform(x_test_sc)
+y_test  = sc_y.fit_transform(y_test_sc)
 
 print('Training Features Shape:', x_train.shape)
 print('Training Labels Shape:', y_train.shape)
@@ -79,32 +89,32 @@ print('Testing Features Shape:', x_test.shape)
 print('Testing Labels Shape:', y_test.shape)
 
 # kNN
-hyper_params = [{'algorithm': ('ball_tree', 'kd_tree', 'brute',), 'n_neighbors': (1,2,3,4,5,6,7,8,9,10,),
-                 'leaf_size': (1, 10, 20, 30, 100,), 'weights': ('uniform', 'distance',), 'p': (1,2,),}]
+hyper_params = [{'algorithm': ('ball_tree', 'kd_tree', 'brute',),
+                 'n_neighbors': (1,2,3,4,5,6,7,8,9,10,),
+                 'leaf_size': (1, 10, 20, 30, 100,),
+                 'weights': ('uniform', 'distance',),
+                 'p': (1,2,),}]
 
-#est=neighbors.NearestNeighbors()
 est=neighbors.KNeighborsRegressor()
 
-#kr = KernelRidge(kernel='rbf', gamma=0.1)
-#kr = GridSearchCV(KernelRidge(kernel='rbf', gamma=0.1), param_grid={"alpha": [1e0, 0.1, 1e-2, 1e-3], "gamma": np.logspace(-2, 2, 5)})
-grid_clf = GridSearchCV(est, cv=5, param_grid=hyper_params, verbose=2, n_jobs=n_jobs, scoring='r2')
+gs = GridSearchCV(est, cv=10, param_grid=hyper_params, verbose=2, n_jobs=n_jobs, scoring='r2')
 
 t0 = time.time()
-grid_clf.fit(x_train, y_train.ravel())
+gs.fit(x_train, y_train.ravel())
 runtime = time.time() - t0
 print("kNN complexity and bandwidth selected and model fitted in %.3f s" % runtime)
 
-train_score_mse = mean_squared_error(sc_y.inverse_transform(y_train), sc_y.inverse_transform(grid_clf.predict(x_train)))
-train_score_mae = mean_absolute_error(sc_y.inverse_transform(y_train),sc_y.inverse_transform(grid_clf.predict(x_train)))
-train_score_evs = explained_variance_score(sc_y.inverse_transform(y_train), sc_y.inverse_transform(grid_clf.predict(x_train)))
-train_score_me  = max_error(sc_y.inverse_transform(y_train), sc_y.inverse_transform(grid_clf.predict(x_train)))
+train_score_mse = mean_squared_error(      sc_y.inverse_transform(y_train), sc_y.inverse_transform(gs.predict(x_train)))
+train_score_mae = mean_absolute_error(     sc_y.inverse_transform(y_train), sc_y.inverse_transform(gs.predict(x_train)))
+train_score_evs = explained_variance_score(sc_y.inverse_transform(y_train), sc_y.inverse_transform(gs.predict(x_train)))
+train_score_me  = max_error(               sc_y.inverse_transform(y_train), sc_y.inverse_transform(gs.predict(x_train)))
 
-test_score_mse  = mean_squared_error(sc_y.inverse_transform(y_test),  sc_y.inverse_transform(grid_clf.predict(x_test)))
-test_score_mae  = mean_absolute_error(sc_y.inverse_transform(y_test), sc_y.inverse_transform(grid_clf.predict(x_test)))
-test_score_evs  = explained_variance_score(sc_y.inverse_transform(y_test),  sc_y.inverse_transform(grid_clf.predict(x_test)))
-test_score_me   = max_error(sc_y.inverse_transform(y_test),  sc_y.inverse_transform(grid_clf.predict(x_test)))
+test_score_mse = mean_squared_error(      sc_y.inverse_transform(y_test), sc_y.inverse_transform(gs.predict(x_test)))
+test_score_mae = mean_absolute_error(     sc_y.inverse_transform(y_test), sc_y.inverse_transform(gs.predict(x_test)))
+test_score_evs = explained_variance_score(sc_y.inverse_transform(y_test), sc_y.inverse_transform(gs.predict(x_test)))
+test_score_me  = max_error(               sc_y.inverse_transform(y_test), sc_y.inverse_transform(gs.predict(x_test)))
 
-sorted_grid_params = sorted(grid_clf.best_params_.items(), key=operator.itemgetter(0))
+sorted_grid_params = sorted(gs.best_params_.items(), key=operator.itemgetter(0))
 
 out_text = '\t'.join(['k-nearest-neighbour',
                       str(trial),
@@ -121,11 +131,11 @@ out_text = '\t'.join(['k-nearest-neighbour',
 print(out_text)
 sys.stdout.flush()
 
-best_algorithm = grid_clf.best_params_['algorithm']
-best_n_neighbors = grid_clf.best_params_['n_neighbors']
-best_leaf_size = grid_clf.best_params_['leaf_size']
-best_weights = grid_clf.best_params_['weights']
-best_p = grid_clf.best_params_['p']
+best_algorithm = gs.best_params_['algorithm']
+best_n_neighbors = gs.best_params_['n_neighbors']
+best_leaf_size = gs.best_params_['leaf_size']
+best_weights = gs.best_params_['weights']
+best_p = gs.best_params_['p']
 
 outF = open("output.txt", "w")
 print('best_algorithm = ', best_algorithm, file=outF)
@@ -135,8 +145,11 @@ print('best_weights = ', best_weights, file=outF)
 print('best_p = ', best_p, file=outF)
 outF.close()
 
-kn = KNeighborsRegressor(n_neighbors=best_n_neighbors, algorithm=best_algorithm,
-                         leaf_size=best_leaf_size, weights=best_weights, p=best_p)
+kn = KNeighborsRegressor(n_neighbors=best_n_neighbors,
+                         algorithm=best_algorithm,
+                         leaf_size=best_leaf_size,
+                         weights=best_weights,
+                         p=best_p)
 
 t0 = time.time()
 kn.fit(x_train, y_train.ravel())