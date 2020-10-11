a5be6753c1c4c71860987e18c76f6aaabfcf0488
lcampoli <l.kampoli@spbu.ru>
2020-10-11 16:50:17 +0300

    added ExtraTrees

---
+++ Regression/TransportCoefficients/shear/src/ExtraTrees/regression.py
@@ -0,0 +1,195 @@
+#!/usr/bin/env python
+
+# https://stackoverflow.com/questions/45074698/how-to-pass-elegantly-sklearns-gridseachcvs-best-parameters-to-another-model
+# https://medium.com/@alexstrebeck/training-and-testing-machine-learning-models-e1f27dc9b3cb
+
+import time
+import sys
+sys.path.insert(0, '../../../../../Utilities/')
+
+from plotting import newfig, savefig
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import matplotlib.gridspec as gridspec
+from mpl_toolkits.axes_grid1 import make_axes_locatable
+
+import numpy as np
+import pandas as pd
+import seaborn as sns
+
+import operator
+import itertools
+
+from sklearn import metrics
+from sklearn.metrics import *
+
+from sklearn.preprocessing import StandardScaler
+
+from sklearn.model_selection import train_test_split, GridSearchCV, KFold, cross_val_score
+
+from sklearn.inspection import permutation_importance
+
+from sklearn import ensemble
+from sklearn.ensemble import ExtraTreesRegressor
+
+from joblib import dump, load
+import pickle
+
+n_jobs = -1
+trial  = 1
+
+with open('../../../../Data/TCs_air5.txt') as f:
+    lines = (line for line in f if not line.startswith('#'))
+    dataset = np.loadtxt(lines, skiprows=1)
+
+x = dataset[:,0:7] # T, P, x_N2, x_O2, x_NO, x_N, x_O
+y = dataset[:,7:8] # shear
+
+x_train, x_test, y_train, y_test = train_test_split(x, y, train_size=0.75, test_size=0.25, random_state=69)
+
+sc_x = StandardScaler()
+sc_y = StandardScaler()
+
+# fit scaler
+sc_x.fit(x_train)
+
+# transform training dataset
+x_train = sc_x.transform(x_train)
+
+# transform test dataset
+x_test = sc_x.transform(x_test)
+
+# fit scaler on training dataset
+sc_y.fit(y_train)
+
+# transform training dataset
+y_train = sc_y.transform(y_train)
+
+# transform test dataset
+y_test = sc_y.transform(y_test)
+
+dump(sc_x, open('scaler_x_shear.pkl', 'wb'))
+dump(sc_y, open('scaler_y_shear.pkl', 'wb'))
+
+print('Training Features Shape:', x_train.shape)
+print('Training Labels Shape:', y_train.shape)
+print('Testing Features Shape:', x_test.shape)
+print('Testing Labels Shape:', y_test.shape)
+
+# Extra Trees
+hyper_params = [{'n_estimators': (1, 100,),
+#                'min_weight_fraction_leaf': (0.0, 0.25, 0.5,),
+                 'min_weight_fraction_leaf': (0.0,),
+#                'max_features': ('sqrt','log2','auto', None,),
+#                'max_samples': (1,10,100,1000,),
+#                'bootstrap': (True, False,),
+#                'oob_score': (True, False,),
+#                'warm_start': (True, False,),
+#                'criterion': ('mse', 'mae',),
+#                'max_depth': (1,10,100,None,),
+                 'max_depth': (None,),
+                 'max_leaf_nodes': (2, 100,),
+                 'min_samples_split': (10,),
+                 'min_samples_leaf': (1,),
+#                'min_samples_leaf': (1,10,100,),
+}]
+
+est=ensemble.ExtraTreesRegressor()
+gs = GridSearchCV(est, cv=10, param_grid=hyper_params, verbose=2, n_jobs=n_jobs, scoring='r2')
+
+#cross_val = KFold(n_splits=3, random_state=69)
+#scores = cross_val_score(model, x_train, y_train, cv=cross_val, scoring='r2')
+#print("Mean R2 Score: ", scores.mean()
+
+t0 = time.time()
+gs.fit(x_train, y_train.ravel())
+runtime = time.time() - t0
+print("Complexity and bandwidth selected and model fitted in %.6f s" % runtime)
+
+train_score_mse = mean_squared_error(      sc_y.inverse_transform(y_train), sc_y.inverse_transform(gs.predict(x_train)))
+train_score_mae = mean_absolute_error(     sc_y.inverse_transform(y_train), sc_y.inverse_transform(gs.predict(x_train)))
+train_score_evs = explained_variance_score(sc_y.inverse_transform(y_train), sc_y.inverse_transform(gs.predict(x_train)))
+train_score_me  = max_error(               sc_y.inverse_transform(y_train), sc_y.inverse_transform(gs.predict(x_train)))
+train_score_r2  = r2_score(                sc_y.inverse_transform(y_train), sc_y.inverse_transform(gs.predict(x_train)))
+
+test_score_mse  = mean_squared_error(      sc_y.inverse_transform(y_test),  sc_y.inverse_transform(gs.predict(x_test)))
+test_score_mae  = mean_absolute_error(     sc_y.inverse_transform(y_test),  sc_y.inverse_transform(gs.predict(x_test)))
+test_score_evs  = explained_variance_score(sc_y.inverse_transform(y_test),  sc_y.inverse_transform(gs.predict(x_test)))
+test_score_me   = max_error(               sc_y.inverse_transform(y_test),  sc_y.inverse_transform(gs.predict(x_test)))
+test_score_r2   = r2_score(                sc_y.inverse_transform(y_test),  sc_y.inverse_transform(gs.predict(x_test)))
+
+print()
+print("The model performance for training set")
+print("--------------------------------------")
+print('MAE is {}'.format(train_score_mae))
+print('MSE is {}'.format(train_score_mse))
+print('EVS is {}'.format(train_score_evs))
+print('ME is {}'.format(train_score_me))
+print('R2 score is {}'.format(train_score_r2))
+print()
+print("The model performance for testing set")
+print("--------------------------------------")
+print('MAE is {}'.format(test_score_mae))
+print('MSE is {}'.format(test_score_mse))
+print('EVS is {}'.format(test_score_evs))
+print('ME is {}'.format(test_score_me))
+print('R2 score is {}'.format(test_score_r2))
+print()
+print("Best parameters set found on development set:")
+print(gs.best_params_)
+print()
+
+# Re-train with best parameters
+regr = ExtraTreesRegressor(**gs.best_params_)
+
+t0 = time.time()
+regr.fit(x_train, y_train.ravel())
+regr_fit = time.time() - t0
+print("Complexity and bandwidth selected and model fitted in %.6f s" % regr_fit)
+
+t0 = time.time()
+y_regr = regr.predict(x_test)
+regr_predict = time.time() - t0
+print("Prediction for %d inputs in %.6f s" % (x_test.shape[0], regr_predict))
+
+with open('output.txt', 'w') as f:
+    print("Training time: %.6f s" % regr_fit, file=f)
+    print("Prediction time: %.6f s" % regr_predict, file=f)
+    print(" ", file=f)
+    print("The model performance for training set", file=f)
+    print("--------------------------------------", file=f)
+    print('MAE is {}'.format(train_score_mae), file=f)
+    print('MSE is {}'.format(train_score_mse), file=f)
+    print('EVS is {}'.format(train_score_evs), file=f)
+    print('ME is {}'.format(train_score_me), file=f)
+    print('R2 score is {}'.format(train_score_r2), file=f)
+    print(" ", file=f)
+    print("The model performance for testing set", file=f)
+    print("--------------------------------------", file=f)
+    print('MAE is {}'.format(test_score_mae), file=f)
+    print('MSE is {}'.format(test_score_mse), file=f)
+    print('EVS is {}'.format(test_score_evs), file=f)
+    print('ME is {}'.format(test_score_me), file=f)
+    print('R2 score is {}'.format(test_score_r2), file=f)
+    print(" ", file=f)
+    print("Best parameters set found on development set:", file=f)
+    print(gs.best_params_, file=f)
+
+x_test_dim = sc_x.inverse_transform(x_test)
+y_test_dim = sc_y.inverse_transform(y_test)
+y_regr_dim = sc_y.inverse_transform(y_regr)
+
+plt.scatter(x_test_dim[:,0], y_test_dim[:], s=2, c='k', marker='o', label='Matlab')
+plt.scatter(x_test_dim[:,0], y_regr_dim[:], s=2, c='r', marker='+', label='ExtraTrees')
+plt.ylabel(r'$\eta$ [Pa·s]')
+plt.xlabel('T [K] ')
+plt.legend()
+plt.tight_layout()
+plt.savefig("shear.pdf", dpi=150, crop='false')
+plt.show()
+plt.close()
+
+# save the model to disk
+dump(gs, 'model_shear.sav')
+
+# https://stackoverflow.com/questions/37161563/how-to-graph-grid-scores-from-gridsearchcv
