bfbd8462e3e4c58f8b38dbafe4487a03f65b4494
lcampoli <l.kampoli@spbu.ru>
2020-11-03 16:25:10 +0300

    added data and few scripts for Rosneft Classification

--- Classification/README.md
+++ Classification/README.md
@@ -1,1 +1,12 @@
 https://boosters.pro/championship/seismic_challenge/data
+
+
+### Useful Files
+* train_images.zip
+* train(masks).csv
+* test_images.zip
+* utils.py
+* seismic_prepare_data.ipynb
+
+### Useful Tools
+* Neural Matlab Toolbox
---
+++ Classification/seismic_challenge_utils.py
@@ -0,0 +1,30 @@
+import numpy as np
+
+
+def rle2mask(mask_rle, shape):
+    '''
+    mask_rle: run-length as string formated (start length)
+    shape: (width,height) of array to return
+    Returns numpy array, 1 - mask, 0 - background
+
+    '''
+    s = mask_rle.split()
+    starts, lengths = [np.asarray(x, dtype=int) for x in (s[0:][::2], s[1:][::2])]
+    starts -= 1
+    ends = starts + lengths
+    img = np.zeros(shape[0] * shape[1], dtype=np.uint8)
+    for lo, hi in zip(starts, ends):
+        img[lo:hi] = 1
+    return img.reshape(shape).T
+
+
+def mask2rle(x):
+    dots = np.where(x.T.flatten() == 1)[0]
+    run_lengths = []
+    prev = -2
+    for b in dots:
+        if b > prev + 1:
+            run_lengths.extend((b + 1, 0))
+        run_lengths[-1] += 1
+        prev = b
+    return run_lengths
