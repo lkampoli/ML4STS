95150563844091521b07974bd8966c7b90fc0c18
lcampoli <l.kampoli@spbu.ru>
2020-11-13 15:26:32 +0300

    fix in_con()

--- Euler_1d_shock_STS/matlab2julia/in_con.jl
+++ Euler_1d_shock_STS/matlab2julia/in_con.jl
@@ -11,22 +11,18 @@ xa = xc[2]
 C1 = sum(m.*xc)*v0^2/(k*T0)
 C2 = 0.5*C1
 
-#S = [n*v == 1,...
-#     n*t+n*v^2*C1 == 1+C1,...
-#     3.5*xm*t+2.5*xa*t+v^2*C2 == 3.5*xm+2.5*xa+C2]
-#
-#N = vpasolve(S,[n,t,v],[10,100,0.1])
-#X = [N.n N.t N.v]
-#sol = double(X)
-#
-#sol1 = sol([1 3 5])
-#sol2 = sol([2 4 6])
-#
-#if any(sol1-1)
+S = [n*v ⩵ 1, n*t+n*v^2*C1 ⩵ (1+C1), 3.5*xm*t+2.5*xa*t+v^2*C2 ⩵ (3.5*xm+2.5*xa+C2)]
+N = SymPy.solve(S)
+
+sol1 = collect(values(N[2]))
+sol2 = collect(values(N[1]))
+
+#FIXME
+#if any(sol1 != 0)
 #  Y = sol1
-#elseif any(sol2-1)
-#  Y = sol
+#elseif any(sol2 != 0)
+#  Y = sol2
 #end
-Y = zeros(3)
-return Y
+
+return sol1
 end
--- Euler_1d_shock_STS/matlab2julia/main.jl
+++ Euler_1d_shock_STS/matlab2julia/main.jl
@@ -1,6 +1,22 @@
 
-#using Unitful
+using Unitful
 using BenchmarkTools
+using MAT
+using SymPy;
+using Plots
+using UnicodePlots
+using PyPlot
+using Images
+using JLD
+using DifferentialEquations
+using StaticArrays
+using ModelingToolkit
+using SparsityDetection
+using SparseArrays
+using LinearAlgebra
+#using DiffEqOperators
+#using AlgebraicMultigrid
+#using Sundials
 
 # Switch for species: 1 - N2/N; 2 - O2/O
 sw_sp = 1
@@ -24,26 +40,24 @@ N_a   = 6.0221e23;  #AvogadroConstant
 R     = 8.3145;     #MolarGasConstant
 h_bar = h/(2*pi);   #PlanckConstantOver2pi
 
-# To load the module to read .mat files
-using MAT
 
 # To read a single variable from a MAT file (compressed files are detected and handled automatically):
 file  = matopen("data_species.mat")
-OMEGA = read(file, "OMEGA");
-BE    = read(file, "BE")   ;
+OMEGA = read(file, "OMEGA"); # Ω
+BE    = read(file, "BE")   ; # Bₑ
 ED    = read(file, "ED")   ;
 CArr  = read(file, "CArr") ;
 NArr  = read(file, "NArr") ;
 QN    = read(file, "QN")   ;
-MU    = read(file, "MU")   ;
+MU    = read(file, "MU")   ; # μ
 R0    = read(file, "R0")   ;
 EM    = read(file, "EM")   ;
 RE    = read(file, "RE")   ;
 close(file)
 
-om_e   = OMEGA[sw_sp,1];
-om_x_e = OMEGA[sw_sp,2];
-Be     = BE[sw_sp]     ;
+om_e   = OMEGA[sw_sp,1]; # ωₑ
+om_x_e = OMEGA[sw_sp,2]; # ωₓₑ
+Be     = BE[sw_sp]     ; # Bₑ
 D      = ED[sw_sp]     ;
 CA     = CArr[sw_sp,:] ;
 nA     = NArr[sw_sp,:] ;
@@ -71,121 +85,112 @@ em     = [EM[sw_sp,1] sqrt(EM[sw_sp,1]*EM[sw_sp,2]*R0[sw_sp,1]^6*R0[sw_sp,2]^6)/
 re     = RE[sw_sp]
 
 # ICs
-p0  = 0.8*133.322
-T0  = 300.
-Tv0 = T0
-M0  = 13.4
-n0  = p0/(k*T0)
+p0  = 0.8*133.322 # p₀
+T0  = 300.        # T₀
+Tv0 = T0          # Tᵥ₀
+M0  = 13.4        # M₀
+n0  = p0/(k*T0)   # n₀
 
 if xc[1] != 0
-  gamma0 = 1.4
+  gamma0 = 1.4    # γ₀
 else
   gamma0 = 5/3
 end
 
 rho0_c = m.*xc*n0
-rho0 = sum(rho0_c)
+rho0   = sum(rho0_c)           # ρ₀
 mu_mix = sum(rho0_c./mu)/rho0
-R_bar = R*mu_mix
-a0 = sqrt(gamma0*R_bar*T0)
-v0 = M0*a0
+R_bar  = R*mu_mix
+a0     = sqrt(gamma0*R_bar*T0) # a₀
+v0     = M0*a0                 # v₀
 
-#using SymPy
-#include("in_con.jl")
-#NN = in_con()
-n1 = 1 #NN[1]
-T1 = 1 #NN[2]
-v1 = 1 #NN[3]
+include("in_con.jl")
+NN = in_con()
+T1 = 1 #NN[1]
+v1 = 1 #NN[2]
+n1 = 1 #NN[3]
+println("T1 = ", T1, "\n", "v1 = ", v1, "\n", "n1 = ", n1, "\n")
 
 Zvibr_0 = sum(exp.(-e_i./Tv0./k))
 
-Y0_bar = zeros(l+3)
+Y0_bar      = zeros(l+3)
 Y0_bar[1:l] = xc[1]*n1/Zvibr_0*exp.(-e_i./Tv0./k)
 Y0_bar[l+1] = xc[2]*n1
 Y0_bar[l+2] = v1
 Y0_bar[l+3] = T1
+println("Y0_bar = ", Y0_bar, "\n")
 
-Delta = 1/(sqrt(2)*n0*sigma0)
-xspan = [0, x_w]./Delta
+Delta = 1/(sqrt(2)*n0*sigma0); println("Delta = ", Delta, "\n")
+xspan = [0, x_w]./Delta;       println("xspan = ", xspan, "\n")
 
-#using Plots
-#using UnicodePlots
-using PyPlot
-using Images
-using JLD
-using DifferentialEquations
-using StaticArrays
-#using AbstractArray
-using ModelingToolkit
-using SparsityDetection, SparseArrays
-using LinearAlgebra
-#using DiffEqOperators
-#using AlgebraicMultigrid
-#using Sundials
 include("rpart.jl")
 include("kdis.jl")
 include("kvt_ssh.jl")
 include("kvv_ssh.jl")
 prob = ODEProblem(rpart!, Y0_bar, xspan)
-sol = solve(prob, Tsit5(), reltol=1e-8, abstol=1e-8)
+#sol  = DifferentialEquations.solve(prob, alg_hints=[:stiff], reltol=1e-8, abstol=1e-8, save_everystep=true)
+sol  = DifferentialEquations.solve(prob, Tsit5(), reltol=1e-8, abstol=1e-8, save_everystep=true)
 
-#x_s = X*Delta*100;
-#Temp = Y(:,l+3)*T0;
-#v = Y(:,l+2)*v0;
-#n_i = Y(:,1:l)*n0;
-#n_a = Y(:,l+1)*n0;
-#n_m = sum(n_i,2);
-#time_s = X*Delta/v0;
-#Npoint = length(X);
-#Nall = sum(n_i,2)+n_a;
-#ni_n = n_i./repmat(Nall,1,l);
-#nm_n = sum(ni_n,2);
-#na_n = n_a./Nall;
-#rho = m(1)*n_m + m(2)*n_a;
-#p = Nall*k.*Temp;
-#e_v = repmat(e_i'+e_0,Npoint,1).*n_i;
-#e_v = sum(e_v,2);
-#e_v0 = n0*xc(1)/Zvibr_0*sum(exp(-e_i/Tv0/k).*(e_i+e_0));
-#e_f = 0.5*D*n_a*k;
-#e_f0 = 0.5*D*xc(2)*n0*k;
-#e_tr = 1.5*Nall*k.*Temp;
-#e_tr0 = 1.5*n0*k.*T0;
-#e_rot = n_m*k.*Temp;
-#e_rot0 = n0*xc(1)*k.*T0;
-#E = e_tr+e_rot+e_v+e_f;
-#E0 = e_tr0+e_rot0+e_v0+e_f0;
-#H = (E+p)./rho;
-#H0 = (E0+p0)./rho0;
-#u10 = rho0*v0;
-#u20 = rho0*v0^2+p0;
-#u30 = H0+v0^2/2;
-#u1 = u10-rho.*v;
-#u2 = u20-rho.*v.^2-p;
-#u3 = u30-H-v.^2/2;
-#d1 = max(abs(u1)/u10);
-#d2 = max(abs(u2)/u20);
-#d3 = max(abs(u3)/u30);
-#disp('Relative error of conservation law of:');
-#disp(['mass = ',num2str(d1)]);
-#disp(['momentum = ',num2str(d2)]);
-#disp(['energy = ',num2str(d3)]);
+X = sol.t
 
-#RDm = zeros(Npoint,l);
-#RDa = zeros(Npoint,l);
-#RVTm = zeros(Npoint,l);
-#RVTa = zeros(Npoint,l);
-#RVV = zeros(Npoint,l);
-#
+x_s    = X*Delta*100;
+Temp   = sol[l+3,:]*T0; println("Temp = ", Temp, "\n")
+v      = sol[l+2,:]*v0; println("v = ", v, "\n")
+n_i    = sol[1:l,:]*n0; println("n_i = ", n_i, "\n", "Size of n_i = ", size(n_i), "\n")
+n_a    = sol[l+1,:]*n0; println("n_a = ", n_a, "\n")
+n_m    = sum(n_i,1);    println("n_m = ", n_m, "\n")
+time_s = X*Delta/v0;    println("time_s = ", time_s, "\n")
+Npoint = length(X);     println("Npoint = ", Npoint, "\n")
+Nall   = sum(n_i,2)+n_a;
+ni_n   = n_i ./ repmat(Nall,1,l);
+nm_n   = sum(ni_n,2);
+na_n   = n_a ./ Nall;
+rho    = m[1]*n_m + m[2]*n_a;
+p      = Nall*k .* Temp;
+e_v    = repmat(e_i+e_0,Npoint,1) .* n_i;
+e_v    = sum(e_v,2);
+e_v0   = n0*xc[1]/Zvibr_0*sum(exp.(-e_i./Tv0/k) .* (e_i+e_0));
+e_f    = 0.5*D*n_a*k;
+e_f0   = 0.5*D*xc[2]*n0*k;
+e_tr   = 1.5*Nall*k .* Temp;
+e_tr0  = 1.5*n0*k .* T0;
+e_rot  = n_m*k .* Temp;
+e_rot0 = n0*xc[1]*k .* T0;
+E      = e_tr+e_rot+e_v+e_f;
+E0     = e_tr0+e_rot0+e_v0+e_f0;
+H      = (E+p) ./ rho;
+H0     = (E0+p0) ./ rho0;
+u10    = rho0*v0;
+u20    = rho0*v0^2+p0;
+u30    = H0+v0^2/2;
+u1     = u10-rho .* v;
+u2     = u20-rho .* v.^2-p;
+u3     = u30-H-v.^2/2;
+d1     = max(abs(u1)/u10);
+d2     = max(abs(u2)/u20);
+d3     = max(abs(u3)/u30);
+
+display("Relative error of conservation law of:");
+println("mass = ", d1);
+println("momentum = ", d2);
+println("energy = ", d3);
+
+RDm  = zeros(Npoint,l);
+RDa  = zeros(Npoint,l);
+RVTm = zeros(Npoint,l);
+RVTa = zeros(Npoint,l);
+RVV  = zeros(Npoint,l);
+
 #for i = 1:Npoint
-#    input = Y(i,:)';
-#    [rdm, rda, rvtm, rvta, rvv] = rpart_post(input)
-#    RDm(i,:) = rdm;
-#    RDa(i,:) = rda;
-#    RVTm(i,:) = rvtm;
-#    RVTa(i,:) = rvta;
-#    RVV(i,:) = rvv;
+# input = Y[i,:];
+# rdm, rda, rvtm, rvta, rvv = rpart_post(input)
+# RDm[i,:]  = rdm;
+# RDa[i,:]  = rda;
+# RVTm[i,:] = rvtm;
+# RVTa[i,:] = rvta;
+# RVV[i,:]  = rvv;
 #end
-#
-#RD_mol = RDm+RDa;
-#RVT    = RVTm+RVTa;
-#RD_at  = -2*sum(RD_mol,2);
+
+RD_mol = RDm+RDa;
+RVT    = RVTm+RVTa;
+RD_at  = -2*sum(RD_mol,2);
